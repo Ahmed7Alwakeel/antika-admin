7222c40e4929a04198446bbde1d7cafe2224598a
Ahmed0Alwakeel <aalwakeel@beyond-creation.net>
2024-07-07 17:53:13 +0300

    handle routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,24 +2,31 @@
 import '../styles/app.scss';
 import '../styles/app-rtl.scss';
 import Layout from '../components/layout/Layout';
-import { BrowserRouter as Router, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Providers from './Providers';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import Login from './pages/auth/Login';
+import Home from './Home';
 
-function App() {
+const App: React.FC = () => {
 
   const { i18n } = useTranslation();
   useEffect(() => {
-		document.documentElement.dir = i18n.dir();
-		document.documentElement.lang = i18n.language;
-	}, [i18n.language]);
+    document.documentElement.dir = i18n.dir();
+    document.documentElement.lang = i18n.language;
+  }, [i18n.language]);
+
+
 
   return (
     <Router>
       <Providers>
         <Layout>
-          <div></div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth/login" element={<Login />} />
+          </Routes>
         </Layout>
       </Providers>
     </Router>
